f36b5ca0323ca8b23ca9ffccf591014774d59097
Zafar Saleem <zafarsaleem3@gmail.com>
2019-06-24 12:46:05 -0400

    onHandleClose function triggered upon close button click

--- src/components/common/dialog/dialog.js
+++ src/components/common/dialog/dialog.js
@@ -12,7 +12,7 @@ class Dialog extends Component {
       <div>
         <div className={(this.props.onShow) ? 'backdrop show' : 'backdrop hide'} onClick={this.props.onHandleClose}></div>
         <div className={(this.props.onShow) ? 'dialog show' : 'dialog hide'}>
-          <button className='btn btn-close'>close</button>
+          <button className='btn btn-close' onClick={this.props.onHandleClose}>close</button>
           <form onSubmit={this.props.onHandleAction}>
             <div>
               <label htmlFor='name'>Name</label>
