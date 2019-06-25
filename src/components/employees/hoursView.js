df657b5ca7a206ed8b4d8bbf7e77a9353e6fc501
Zafar Saleem <zafarsaleem3@gmail.com>
2019-06-25 17:23:07 -0400

    marked all fields necessary

--- src/components/employees/hoursView.js
+++ src/components/employees/hoursView.js
@@ -5,19 +5,19 @@ const HoursView = ({ success, message, onFileHours, isShow, onBlur, onFocus }) =
     <div className={(success) ? 'success' : 'error'}>{message}</div>
     <form onSubmit={onFileHours}>
       <div>
-        <label htmlFor="date">Date</label>
+        <label htmlFor="date">Date*</label>
         <input type="date" name="date" id="date" />
       </div>
       <div>
-        <label htmlFor="start-time">Start Time</label>
+        <label htmlFor="start-time">Start Time*</label>
         <input type="time" name="startTime" id="start-time" />
       </div>
       <div>
-        <label htmlFor="finish-time">Finish Time</label>
+        <label htmlFor="finish-time">Finish Time*</label>
         <input type="time" name="finishTime" id="finish-time" />
       </div>
       <div>
-        <label className="top" htmlFor="tasks">List of Tasks</label>
+        <label className="top" htmlFor="tasks">List of Tasks*</label>
         <textarea cols="40" rows="10" name="tasks" id="tasks" onClick={onFocus} onBlur={onBlur}></textarea>
         <span className={(isShow) ? 'action-indicator show' : 'action-indicator hide'}>Enter Password & Hit Enter to Save</span>
       </div>
